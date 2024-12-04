f03f70dfbce0836f6a5b7145a4fbe36666da0f51
Sathya Tadinada <sathyav.tadinada@gmail.com>
2024-12-04 12:02:28 -0700

    Updated gitignore and initialized day5

---
+++ day5/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let lines = include_str!("input.txt").lines();
+
+    part1(lines.clone());
+    part2(lines.clone());
+}
+
+fn part1(lines: std::str::Lines) {
+    let mut answer = 0;
+
+    println!("Part 1: {}", answer);
+}
+
+fn part2(lines: std::str::Lines) {
+    let mut answer = 0;
+
+    println!("Part 2: {}", answer);
+}
+
