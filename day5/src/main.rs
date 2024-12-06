6c085b86b58157c0885aaf8e14d749c270f9b263
Sathya Tadinada <sathyav.tadinada@gmail.com>
2024-12-06 01:20:07 -0700

    Refactored day5, finished day6

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,19 +1,83 @@
 fn main() {
-    let lines = include_str!("input.txt").lines();
+    let arr = include_str!("input.txt").lines().map(|x| x.to_string()).collect();
+    let (rules, pages) = parse(arr);
 
-    part1(lines.clone());
-    part2(lines.clone());
+    println!("{}", part1(&rules, &pages));
+    println!("{}", part2(&rules, &pages));
 }
 
-fn part1(lines: std::str::Lines) {
-    let mut answer = 0;
-
-    println!("Part 1: {}", answer);
+fn part1(rules: &[(i32, i32)], pages: &[Vec<i32>]) -> i32 {
+    let mut sum = 0;
+    for row in pages {
+        if is_ordered(rules, row) {
+            let middle_index = row.len() / 2;
+            sum += row[middle_index];
+        }
+    }
+    sum
 }
 
-fn part2(lines: std::str::Lines) {
-    let mut answer = 0;
-
-    println!("Part 2: {}", answer);
+fn part2(rules: &[(i32, i32)], pages: &[Vec<i32>]) -> i32 {
+    let mut sum = 0;
+    for row in pages {
+        if !is_ordered(rules, row) {
+            let new_row = re_order(rules, &mut row.clone());
+            let middle_index = new_row.len() / 2;
+            sum += new_row[middle_index];
+        }
+    }
+    sum
 }
 
+fn parse(arr: Vec<String>) -> (Vec<(i32, i32)>, Vec<Vec<i32>>) {
+    let mut rules: Vec<(i32, i32)> = Vec::new();
+    let mut pages: Vec<Vec<i32>> = Vec::new();
+
+    let mut sections = arr.split(|line| line.is_empty());
+    let rules_section = sections.next().unwrap();
+    let pages_section = sections.next().unwrap();
+
+    for line in rules_section {
+        let parts: Vec<i32> = line.split("|").map(|x| x.parse::<i32>().unwrap()).collect();
+        rules.push((parts[0], parts[1]));
+    }
+
+    for line in pages_section {
+        let parts: Vec<i32> = line.split(",").map(|x| x.parse::<i32>().unwrap()).collect();
+        pages.push(parts);
+    }
+    (rules, pages)
+}
+
+fn is_ordered(rules: &[(i32, i32)], row: &[i32]) -> bool {
+    let mut ordered = true;
+    for rule in rules {
+        let (x, y) = rule;
+        if row.contains(x) && row.contains(y) {
+            let x_index = row.iter().position(|&r| r == *x).unwrap();
+            let y_index = row.iter().position(|&r| r == *y).unwrap();
+            ordered &= x_index < y_index;
+        }
+    }
+    ordered
+}
+
+fn re_order(rules: &[(i32, i32)], row: &mut [i32]) -> Vec<i32> {
+    let mut ordered = false;
+    while !ordered {
+        for rule in rules {
+            let (x, y) = rule;
+            if row.contains(x) && row.contains(y) {
+                let x_index = row.iter().position(|&r| r == *x).unwrap();
+                let y_index = row.iter().position(|&r| r == *y).unwrap();
+                if y_index < x_index {
+                    let temp = row[x_index];
+                    row[x_index] = *y;
+                    row[y_index] = temp;
+                }
+            }
+        }
+        ordered = is_ordered(rules, row);
+    }
+    row.to_vec()
+}
--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,96 @@
+use std::collections::HashSet;
+
 fn main() {
-    println!("Hello, world!");
+    let (grid, height, width, x, y) = parse();
+
+    part1(grid.clone(), height, width, x, y);
+    part2(grid.clone(), height, width, x, y);
+}
+
+fn part1(grid: Vec<Vec<char>>, height: usize, width: usize, mut x: usize, mut y: usize) {
+    let directions = [(-1, 0), (0, 1), (1, 0), (0, -1)];
+    let mut dir_index = 0;
+    let mut visited = HashSet::new();
+
+    while x < height && y < width {
+        visited.insert((x, y));
+        loop {
+            let direction = directions[dir_index];
+            let next_x = (x as isize + direction.0) as usize;
+            let next_y = (y as isize + direction.1) as usize;
+            if next_x < height && next_y < width && grid[next_x][next_y] == '#' {
+                dir_index = (dir_index + 1) % 4;
+            } else {
+                x = next_x;
+                y = next_y;
+                break;
+            }
+        }
+    }
+    println!("Part 1: {}", visited.len());
+}
+
+fn part2(mut grid: Vec<Vec<char>>, height: usize, width: usize, x: usize, y: usize) {
+    let mut answer = 0;
+    let directions = [(-1, 0), (0, 1), (1, 0), (0, -1)];
+
+    for i in 0..height {
+        for j in 0..width {
+            if grid[i][j] == '#' || grid[i][j] == '^' || grid[i][j] == 'X' {
+                continue;
+            }
+
+            grid[i][j] = 'X';
+
+            let mut visited = HashSet::new();
+            let (mut current_direction, mut current_x, mut current_y) = (0, x, y);
+
+            while current_x < height && current_y < width && !visited.contains(&(current_x, current_y, current_direction)) {
+                visited.insert((current_x, current_y, current_direction));
+                loop {
+                    let current_dir = directions[current_direction];
+                    let next_x = (current_x as isize + current_dir.0) as usize;
+                    let next_y = (current_y as isize + current_dir.1) as usize;
+                    if next_x < height && next_y < width && (grid[next_x][next_y] == '#' || grid[next_x][next_y] == 'X') {
+                        current_direction = (current_direction + 1) % 4;
+                    } else {
+                        current_x = next_x;
+                        current_y = next_y;
+                        break;
+                    }
+                }
+            }
+
+            if visited.contains(&(current_x, current_y, current_direction)) {
+                answer += 1;
+            }
+
+            grid[i][j] = '.';
+        }
+    }
+
+    println!("Part 2: {}", answer);
+}
+
+fn parse() -> (Vec<Vec<char>>, usize, usize, usize, usize) {
+    let grid: Vec<Vec<char>> = include_str!("input.txt")
+        .lines()
+        .map(|r| r.chars().collect())
+        .collect();
+
+    let height = grid.len();
+    let width = grid[0].len();
+
+    let mut x = 0;
+    let mut y = 0;
+    (0..height).for_each(|i| {
+        (0..width).for_each(|j| {
+            if grid[i][j] == '^' {
+                x = i;
+                y = j;
+            }
+        });
+    });
+
+    (grid, height, width, x, y)
 }
